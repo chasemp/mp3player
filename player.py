168d545ec4e9fb4d4131bed5ff91326654861e26
Chase MP <rush@deviantart.com>
2013-08-22 14:57:27 -0500

    set volume reorg

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 #----------------------------------------------------------------------
-# player_skeleton.py
+# player_skeleton2.py
 #
-# Created: 04/14/2010-04/15/2010
+# Created: 04/15/2010
 #
 # Author: Mike Driscoll - mike@pythonlibrary.org
 #----------------------------------------------------------------------
@@ -9,7 +9,7 @@
 import os
 import wx
 import wx.media
-from wx.lib.agw.shapedbutton import SBitmapButton, SBitmapToggleButton
+import wx.lib.buttons as buttons
 
 dirName = os.path.dirname(os.path.abspath(__file__))
 bitmapDir = os.path.join(dirName, 'bitmaps')
@@ -22,9 +22,19 @@ class MediaPanel(wx.Panel):
     def __init__(self, parent):
         """Constructor"""
         wx.Panel.__init__(self, parent=parent)
+        
+        self.frame = parent
+        self.currentVolume = 50
+        self.createMenu()
         self.layoutControls()
-        self.volume = 50
-
+        
+        sp = wx.StandardPaths.Get()
+        self.currentFolder = sp.GetDocumentsDir()
+        
+        self.timer = wx.Timer(self)
+        self.Bind(wx.EVT_TIMER, self.onTimer)
+        self.timer.Start(100)
+        
     #----------------------------------------------------------------------
     def layoutControls(self):
         """
@@ -32,121 +42,208 @@ class MediaPanel(wx.Panel):
         """
         
         try:
-            self.media = wx.media.MediaCtrl(self, style=wx.SIMPLE_BORDER)
+            self.mediaPlayer = wx.media.MediaCtrl(self, style=wx.SIMPLE_BORDER)
         except NotImplementedError:
             self.Destroy()
             raise
-        
+                
+        # create playback slider
         self.playbackSlider = wx.Slider(self, size=wx.DefaultSize)
-        volumeBtn = wx.Button(self, label="Volume")
+        self.Bind(wx.EVT_SLIDER, self.onSeek, self.playbackSlider)
         
+        self.volumeCtrl = wx.Slider(self, style=wx.SL_VERTICAL|wx.SL_INVERSE)
+        self.volumeCtrl.SetRange(0, 100)
+        self.volumeCtrl.SetValue(self.currentVolume)
+        self.volumeCtrl.Bind(wx.EVT_SLIDER, self.onSetVolume)
+                
         # Create sizers
         mainSizer = wx.BoxSizer(wx.VERTICAL)
         hSizer = wx.BoxSizer(wx.HORIZONTAL)
-        audioTBSizer = self.buildAudioToolBar()
-        
+        audioSizer = self.buildAudioBar()
+                
         # layout widgets
-        hSizer.Add(self.playbackSlider, 0, wx.ALL, 5)
-        hSizer.Add(volumeBtn, 0, wx.ALL, 5)
+        mainSizer.Add(self.playbackSlider, 1, wx.ALL|wx.EXPAND, 5)
+        hSizer.Add(audioSizer, 0, wx.ALL|wx.CENTER, 5)
+        hSizer.Add(self.volumeCtrl, 0, wx.ALL, 5)
         mainSizer.Add(hSizer)
-        mainSizer.Add(audioTBSizer)
+        
         self.SetSizer(mainSizer)
+        self.Layout()
         
     #----------------------------------------------------------------------
-    def buildAudioToolBar(self):
+    def buildAudioBar(self):
         """
-        Based on the ShapedButton demo from the wxPython Demo
+        Builds the audio bar controls
         """
-        hSizer = wx.BoxSizer(wx.HORIZONTAL)
+        audioBarSizer = wx.BoxSizer(wx.HORIZONTAL)
         
-        # The Rewind Button Is A Simple Bitmap Button (SBitmapButton)
-        upbmp = wx.Bitmap(os.path.join(bitmapDir, "rewind.png"), wx.BITMAP_TYPE_PNG)
-        disbmp = wx.Bitmap(os.path.join(bitmapDir, "rewinddisabled.png"), wx.BITMAP_TYPE_PNG)
-        self.rewind = SBitmapButton(self, -1, upbmp, (48, 48))
-        self.rewind.SetUseFocusIndicator(False)
-        self.rewind.SetBitmapDisabled(disbmp)
-
-        self.rewind.Bind(wx.EVT_BUTTON, self.onRewind)
-        hSizer.Add(self.rewind, 0, wx.LEFT, 3)
+        self.buildBtn({'bitmap':'player_prev.png', 'handler':self.onPrev,
+                       'name':'prev'},
+                      audioBarSizer)
         
-        # The Play Button Is A Toggle Bitmap Button (SBitmapToggleButton)
-        upbmp = wx.Bitmap(os.path.join(bitmapDir, "play.png"), wx.BITMAP_TYPE_PNG)
-        disbmp = wx.Bitmap(os.path.join(bitmapDir, "playdisabled.png"), wx.BITMAP_TYPE_PNG)
-        self.play = SBitmapToggleButton(self, -1, upbmp, (48, 48))
-        self.play.SetUseFocusIndicator(False)
-        self.play.SetBitmapDisabled(disbmp)
-
-        self.play.Bind(wx.EVT_BUTTON, self.onPlay)
-        hSizer.Add(self.play, 0, wx.LEFT, 3)
-
-        # The Pause Button Is A Toggle Bitmap Button (SBitmapToggleButton)
-        upbmp = wx.Bitmap(os.path.join(bitmapDir, "pause.png"), wx.BITMAP_TYPE_PNG)
-        disbmp = wx.Bitmap(os.path.join(bitmapDir, "pausedisabled.png"), wx.BITMAP_TYPE_PNG)
-        self.pause = SBitmapToggleButton(self, -1, upbmp, (48, 48))
-        self.pause.SetUseFocusIndicator(False)
-        self.pause.SetBitmapDisabled(disbmp)
-        self.pause.Enable(False)
-
-        self.pause.Bind(wx.EVT_BUTTON, self.onPause)
-        hSizer.Add(self.pause, 0, wx.LEFT, 3)
-
-        # The Stop Button Is A Simple Bitmap Button (SBitmapButton)
-        upbmp = wx.Bitmap(os.path.join(bitmapDir, "stop.png"), wx.BITMAP_TYPE_PNG)
-        disbmp = wx.Bitmap(os.path.join(bitmapDir, "stopdisabled.png"), wx.BITMAP_TYPE_PNG)
-        self.stop = SBitmapButton(self, -1, upbmp, (48, 48))
-        self.stop.SetUseFocusIndicator(False)
-        self.stop.SetBitmapDisabled(disbmp)
-        self.stop.Enable(False)
-
-        self.stop.Bind(wx.EVT_BUTTON, self.onStop)
-        hSizer.Add(self.stop, 0, wx.LEFT, 3)
-
-        # The FFWD Button Is A Simple Bitmap Button (SBitmapButton)
-        upbmp = wx.Bitmap(os.path.join(bitmapDir, "ffwd.png"), wx.BITMAP_TYPE_PNG)
-        disbmp = wx.Bitmap(os.path.join(bitmapDir, "ffwddisabled.png"), wx.BITMAP_TYPE_PNG)
-        self.ffwd = SBitmapButton(self, -1, upbmp, (48, 48))
-        self.ffwd.SetUseFocusIndicator(False)
-        self.ffwd.SetBitmapDisabled(disbmp)
-
-        self.ffwd.Bind(wx.EVT_BUTTON, self.onFFWD)
-        hSizer.Add(self.ffwd, 0, wx.LEFT, 3)
-        return hSizer
+        # create play/pause toggle button
+        img = wx.Bitmap(os.path.join(bitmapDir, "player_play.png"))
+        self.playPauseBtn = buttons.GenBitmapToggleButton(self, bitmap=img, name="play")
+        self.playPauseBtn.Enable(False)
+        
+        img = wx.Bitmap(os.path.join(bitmapDir, "player_pause.png"))
+        self.playPauseBtn.SetBitmapSelected(img)
+        self.playPauseBtn.SetInitialSize()
+        
+        self.playPauseBtn.Bind(wx.EVT_BUTTON, self.onPlay)
+        audioBarSizer.Add(self.playPauseBtn, 0, wx.LEFT, 3)
+        
+        btnData = [{'bitmap':'player_stop.png',
+                    'handler':self.onStop, 'name':'stop'},
+                    {'bitmap':'player_next.png',
+                     'handler':self.onNext, 'name':'next'}]
+        for btn in btnData:
+            self.buildBtn(btn, audioBarSizer)
+            
+        return audioBarSizer
+                    
+    #----------------------------------------------------------------------
+    def buildBtn(self, btnDict, sizer):
+        """"""
+        bmp = btnDict['bitmap']
+        handler = btnDict['handler']
+                
+        img = wx.Bitmap(os.path.join(bitmapDir, bmp))
+        btn = buttons.GenBitmapButton(self, bitmap=img, name=btnDict['name'])
+        btn.SetInitialSize()
+        btn.Bind(wx.EVT_BUTTON, handler)
+        sizer.Add(btn, 0, wx.LEFT, 3)
+        
+    #----------------------------------------------------------------------
+    def createMenu(self):
+        """
+        Creates a menu
+        """
+        menubar = wx.MenuBar()
+        
+        fileMenu = wx.Menu()
+        open_file_menu_item = fileMenu.Append(wx.NewId(), "&Open", "Open a File")
+        menubar.Append(fileMenu, '&File')
+        self.frame.SetMenuBar(menubar)
+        self.frame.Bind(wx.EVT_MENU, self.onBrowse, open_file_menu_item)
+        
+    #----------------------------------------------------------------------
+    def loadMusic(self, musicFile):
+        """"""
+        if not self.mediaPlayer.Load(musicFile):
+            wx.MessageBox("Unable to load %s: Unsupported format?" % path,
+                          "ERROR",
+                          wx.ICON_ERROR | wx.OK)
+        else:
+            self.mediaPlayer.SetInitialSize()
+            self.GetSizer().Layout()
+            self.playbackSlider.SetRange(0, self.mediaPlayer.Length())
+            self.playPauseBtn.Enable(True)
     
     #----------------------------------------------------------------------
-    def onFFWD(self, event):
+    def onBrowse(self, event):
         """
+        Opens file dialog to browse for music
+        """
+        wildcard = "MP3 (*.mp3)|*.mp3|"     \
+                   "WAV (*.wav)|*.wav"
+        dlg = wx.FileDialog(
+            self, message="Choose a file",
+            defaultDir=self.currentFolder, 
+            defaultFile="",
+            wildcard=wildcard,
+            style=wx.OPEN | wx.CHANGE_DIR
+            )
+        if dlg.ShowModal() == wx.ID_OK:
+            path = dlg.GetPath()
+            self.currentFolder = os.path.dirname(path)
+            self.loadMusic(path)
+        dlg.Destroy()
+            
+    #----------------------------------------------------------------------
+    def onNext(self, event):
+        """
+        Not implemented!
         """
         pass
     
     #----------------------------------------------------------------------
-    def onPause(self, event):
+    def onPause(self):
         """
+        Pauses the music
         """
-        pass
+        self.mediaPlayer.Pause()
     
     #----------------------------------------------------------------------
     def onPlay(self, event):
-        """"""
+        """
+        Plays the music
+        """
+        if not event.GetIsDown():
+            self.onPause()
+            return
+        
+        if not self.mediaPlayer.Play():
+            wx.MessageBox("Unable to Play media : Unsupported format?",
+                          "ERROR",
+                          wx.ICON_ERROR | wx.OK)
+        else:
+            self.mediaPlayer.SetInitialSize()
+            self.GetSizer().Layout()
+            self.playbackSlider.SetRange(0, self.mediaPlayer.Length())
+            
+        event.Skip()
+    
+    #----------------------------------------------------------------------
+    def onPrev(self, event):
+        """
+        Not implemented!
+        """
         pass
     
     #----------------------------------------------------------------------
-    def onRewind(self, event):
+    def onSeek(self, event):
         """
+        Seeks the media file according to the amount the slider has
+        been adjusted.
         """
-        pass
+        offset = self.playbackSlider.GetValue()
+        self.mediaPlayer.Seek(offset)
+        
+    #----------------------------------------------------------------------
+    def onSetVolume(self, event):
+        """
+        Sets the volume of the music player
+        """
+        self.currentVolume = self.volumeCtrl.GetValue()
+        print "setting volume to: %s" % int(self.currentVolume)
+        self.mediaPlayer.SetVolume(self.currentVolume)
     
     #----------------------------------------------------------------------
     def onStop(self, event):
         """
+        Stops the music and resets the play button
         """
-        pass
+        self.mediaPlayer.Stop()
+        self.playPauseBtn.SetToggle(False)
+        
+    #----------------------------------------------------------------------
+    def onTimer(self, event):
+        """
+        Keeps the player slider updated
+        """
+        offset = self.mediaPlayer.Tell()
+        self.playbackSlider.SetValue(offset)
 
+########################################################################
 class MediaFrame(wx.Frame):
  
+    #----------------------------------------------------------------------
     def __init__(self):
         wx.Frame.__init__(self, None, wx.ID_ANY, "Python Music Player")
         panel = MediaPanel(self)
         
+#----------------------------------------------------------------------
 # Run the program
 if __name__ == "__main__":
     app = wx.App(False)

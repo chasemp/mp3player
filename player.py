2c757163ba1c600f02443ccad38fd977c901a465
Chase MP <rush@deviantart.com>
2013-08-22 14:59:19 -0500

    handle metadata and track player bar, etc

--- player.py
+++ player.py
@@ -10,6 +10,10 @@ import os
 import wx
 import wx.media
 import wx.lib.buttons as buttons
+#for mp3 meta info grabbing
+from meta import listDirectory
+from meta import MP3FileInfo
+import os.path
 
 dirName = os.path.dirname(os.path.abspath(__file__))
 bitmapDir = os.path.join(dirName, 'bitmaps')
@@ -35,6 +39,7 @@ class MediaPanel(wx.Panel):
         self.Bind(wx.EVT_TIMER, self.onTimer)
         self.timer.Start(100)
         self.list_place = 0
+        self.musicList = []
 
     #----------------------------------------------------------------------
     def layoutControls(self):
@@ -52,20 +57,34 @@ class MediaPanel(wx.Panel):
         self.playbackSlider = wx.Slider(self, size=wx.DefaultSize)
         self.Bind(wx.EVT_SLIDER, self.onSeek, self.playbackSlider)
         
-        self.volumeCtrl = wx.Slider(self, style=wx.SL_VERTICAL|wx.SL_INVERSE)
+        #self.volumeCtrl = wx.Slider(self, style=wx.SL_VERTICAL|wx.SL_INVERSE)
+        self.volumeCtrl = wx.Slider(self, style=wx.HORIZONTAL)
         self.volumeCtrl.SetRange(0, 100)
         self.volumeCtrl.SetValue(self.currentVolume)
         self.volumeCtrl.Bind(wx.EVT_SLIDER, self.onSetVolume)
-                
+      
+
         # Create sizers
         mainSizer = wx.BoxSizer(wx.VERTICAL)
         hSizer = wx.BoxSizer(wx.HORIZONTAL)
         audioSizer = self.buildAudioBar()
                 
+        #ATTEMPT AT SONGTITLE DISPLAY
+        self.now_playing = wx.StaticText(self, wx.ID_ANY, 'Now Playing:', (20, 100))
+        tSizer = wx.BoxSizer(wx.HORIZONTAL)
+        tSizer.Add(self.now_playing, 0, wx.ALL|wx.CENTER, 5)
+        mainSizer.Add(tSizer)
+
+        #ATTEMPT AT SONGTITLE DISPLAY
+        self.songTitle = wx.StaticText(self, wx.ID_ANY, label="Song Title", style=wx.ALIGN_CENTRE)
+        sSizer = wx.BoxSizer(wx.HORIZONTAL)
+        sSizer.Add(self.songTitle, 0, wx.ALL|wx.RIGHT, 5)
+        mainSizer.Add(sSizer)
+
         # layout widgets
         mainSizer.Add(self.playbackSlider, 1, wx.ALL|wx.EXPAND, 5)
         hSizer.Add(audioSizer, 0, wx.ALL|wx.CENTER, 5)
-        hSizer.Add(self.volumeCtrl, 0, wx.ALL, 5)
+        hSizer.Add(self.volumeCtrl, 0, wx.ALL|wx.CENTER, 5)
         mainSizer.Add(hSizer)
         
         self.SetSizer(mainSizer)
@@ -123,10 +142,34 @@ class MediaPanel(wx.Panel):
         menubar = wx.MenuBar()
         
         fileMenu = wx.Menu()
-        open_file_menu_item = fileMenu.Append(wx.NewId(), "&Open", "Open a File")
+
+        open_file_menu_item = fileMenu.Append(wx.NewId(), "&Open", "Open a file")
+        add_file_menu_item = fileMenu.Append(wx.NewId(), "&Add", "Add files")
+
+        menubar.Append(fileMenu, '&File')
+
+        self.frame.SetMenuBar(menubar)
+
+        self.frame.Bind(wx.EVT_MENU, self.onBrowse, open_file_menu_item)
+
+        self.frame.Bind(wx.EVT_MENU, self.onBrowseAdd, add_file_menu_item)
+
+    def createStatusBar(self):
+        """
+        NOT IMPLEMENTED
+        Creates a status bar
+        """
+        statusbar = wx.StatusBar()
+        
+        fileMenu = wx.Menu()
+        open_file_menu_item = fileMenu.Append(wx.NewId(), "&Open", "Open a file")
+        add_file_menu_item = fileMenu.Append(wx.NewId(), "&Add", "Add files")
+
         menubar.Append(fileMenu, '&File')
         self.frame.SetMenuBar(menubar)
         self.frame.Bind(wx.EVT_MENU, self.onBrowse, open_file_menu_item)
+
+        self.frame.Bind(wx.EVT_MENU, self.onBrowseAdd, add_file_menu_item)
         
     #----------------------------------------------------------------------
     def loadMusic(self, musicFile):
@@ -141,16 +184,70 @@ class MediaPanel(wx.Panel):
             self.playbackSlider.SetRange(0, self.mediaPlayer.Length())
             self.playPauseBtn.Enable(True)
 
-        
+        metatag = MP3FileInfo(musicFile)
+
+        if 'artist' in metatag:
+            if 'title' in metatag:
+                songinfo = "%s - %s" % (metatag['artist'], metatag['title'])
+
+        if 'artist' not in metatag:
+            metatag['artist'] = "!ID3"
+
+        if 'title' not in metatag:
+	    title = os.path.basename(musicFile.strip(".mp3"))
+            metatag['title'] = title
+            songinfo = "%s" % (metatag['title'])
+
+
+
+
+        self.songTitle.SetLabel(songinfo)
         self.Play()
 
         print musicFile
-    
+
     #----------------------------------------------------------------------
-    def onBrowse(self, event):
+    def onBrowseAdd(self, event, append=False):
         """
         Opens file dialog to browse for music
         """
+        if append:
+            print "I should append"
+
+        wildcard = "MP3 (*.mp3)|*.mp3|"     \
+                   "WAV (*.wav)|*.wav"
+        dlg = wx.FileDialog(
+            self, message="Choose a file",
+            defaultDir=self.currentFolder, 
+            defaultFile="",
+            wildcard=wildcard,
+            style=wx.OPEN | wx.CHANGE_DIR
+            )
+
+        if dlg.ShowModal() == wx.ID_OK:
+            path = dlg.GetPaths()
+            print "path: " + str(path)
+            self.currentFolder = os.path.dirname(path[0])
+            
+            if self.list_place != 0:
+                self.list_place += 1
+
+            for song in path:
+                print "Adding: " + song
+                self.musicList.append(song)
+
+            #self.allPlay(self.musicList)
+            self.loadMusic(self.musicList[self.list_place])
+
+        dlg.Destroy()
+    #----------------------------------------------------------------------
+    def onBrowse(self, event, append=False):
+        """
+        Opens file dialog to browse for music
+        """
+        if append:
+            print "I should append"
+
         wildcard = "MP3 (*.mp3)|*.mp3|"     \
                    "WAV (*.wav)|*.wav"
         dlg = wx.FileDialog(
@@ -171,15 +268,13 @@ class MediaPanel(wx.Panel):
             for song in self.musicList:
                 print "Music: " + song
 
-            self.loadMusic(path[0])
+            self.loadMusic(path[self.list_place])
 
         dlg.Destroy()
             
     #----------------------------------------------------------------------
-    def onNext(self, event):
-        """
-        Not implemented!
-        """
+
+    def Next(self):
 
         if len(self.musicList) < self.list_place + 2:
             self.list_place = 0
@@ -191,12 +286,17 @@ class MediaPanel(wx.Panel):
         self.loadMusic(self.musicList[self.list_place])
         print "Loading: " + self.musicList[self.list_place] 
 
+        self.Play()
+        pass
+
+    def onNext(self, event):
+
         if not event.GetIsDown():
             self.onPause()
             return
 
-        self.Play()
-        pass
+        self.Next()
+
     
     #----------------------------------------------------------------------
     def onPause(self):
@@ -210,6 +310,7 @@ class MediaPanel(wx.Panel):
         """
         Play the music
         """
+
         if not self.mediaPlayer.Play():
             wx.MessageBox("Unable to Play media : Unsupported format?",
                           "ERROR",
@@ -218,7 +319,10 @@ class MediaPanel(wx.Panel):
             self.mediaPlayer.SetInitialSize()
             self.GetSizer().Layout()
             self.playbackSlider.SetRange(0, self.mediaPlayer.Length())
-    
+
+
+
+
     #----------------------------------------------------------------------
     def onPlay(self, event):
         """
@@ -229,13 +333,24 @@ class MediaPanel(wx.Panel):
             return
         
         self.Play()
-            
+       
         event.Skip()
     
     #----------------------------------------------------------------------
+    def allPlay(self, songList):
+        """
+        Play the music
+        """
+        for song in songList:
+            print "Loading: " + self.musicList[self.list_place] 
+            self.loadMusic(songList[self.list_place])
+            self.list_place += 1
+            self.Play()
+
+
+    #----------------------------------------------------------------------
     def onPrev(self, event):
         """
-        Not implemented!
         """
         if self.list_place == 0:
             self.list_place = len(self.musicList) - 2
@@ -270,8 +385,9 @@ class MediaPanel(wx.Panel):
         """
         self.currentVolume = self.volumeCtrl.GetValue()
         print "setting volume to: %s" % int(self.currentVolume)
-        self.mediaPlayer.SetVolume(self.currentVolume)
-    
+        self.mediaPlayer.SetVolume(float(self.currentVolume) / 100)    
+
+
     #----------------------------------------------------------------------
     def onStop(self, event):
         """
@@ -283,10 +399,27 @@ class MediaPanel(wx.Panel):
     #----------------------------------------------------------------------
     def onTimer(self, event):
         """
-        Keeps the player slider updated
+        Controls continous playback and keeps track of slider
         """
-        offset = self.mediaPlayer.Tell()
-        self.playbackSlider.SetValue(offset)
+        self.offset = self.mediaPlayer.Tell()
+        self.playbackSlider.SetValue(self.offset)
+
+ 	dtotal = self.mediaPlayer.GetDownloadTotal()
+        #print "dtotal " + str(dtotal)
+
+ 	length = self.mediaPlayer.Length()
+        #print "lenght " + str(length)
+
+        #print "offset: " + str(self.offset)
+
+ 	state = self.mediaPlayer.GetState()
+
+
+ 
+        #print "state " + str(state)
+        if state == 0 and self.musicList:
+            self.Next()
+
 
 ########################################################################
 class MediaFrame(wx.Frame):
@@ -295,6 +428,7 @@ class MediaFrame(wx.Frame):
     def __init__(self):
         wx.Frame.__init__(self, None, wx.ID_ANY, "cAMP Music Player")
         panel = MediaPanel(self)
+
         
 #----------------------------------------------------------------------
 # Run the program
